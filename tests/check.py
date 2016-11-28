b04882343ba502c92062fbeb5457f9a96da47997
dakurels <dakurels@gmail.com>
2016-11-28 20:58:35 +0100

    More info in check script

--- tests/check.py
+++ tests/check.py
@@ -1,11 +1,20 @@
 import sys
 
-failure = False
+failures = 0
+number_of_tests = 0
 
 for line in sys.stdin:
     l = line.strip()
     print(l)
+    if l.startswith("Test"):
+        number_of_tests += 1
     if l.startswith('FAIL'):
-        failure = True
-if failure:
+        failures += 1
+
+
+if failures:
+    print("{}/{} tests failed".format(failures, number_of_tests))
     exit(1)
+else:
+    print("All {} tests passed".format(number_of_tests))
+
